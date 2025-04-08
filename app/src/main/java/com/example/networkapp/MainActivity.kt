e89af7cb04f65e0c31b2e181c40ea972cddb7729
plasticono <friedboy07@gmail.com>
2025-04-08 11:32:29 -0400

    Fixed internet permission Implemented save comic Loads comic on restart from file

--- app/src/main/java/com/example/networkapp/MainActivity.kt
+++ app/src/main/java/com/example/networkapp/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.example.networkapp
 
+import android.graphics.Bitmap
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
@@ -7,6 +9,7 @@ import android.widget.EditText
 import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
+import androidx.core.net.toUri
 import com.android.volley.Request
 import com.android.volley.RequestQueue
 import com.android.volley.toolbox.JsonObjectRequest
@@ -27,6 +30,7 @@ class MainActivity : AppCompatActivity() {
     lateinit var numberEditText: EditText
     lateinit var showButton: Button
     lateinit var comicImageView: ImageView
+    lateinit var comicJson: JSONObject
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -40,10 +44,25 @@ class MainActivity : AppCompatActivity() {
         showButton = findViewById<Button>(R.id.showComicButton)
         comicImageView = findViewById<ImageView>(R.id.comicImageView)
 
+        //load comic
+        try {
+            val file = openFileInput("comic.json")
+            val comicObject = JSONObject(file.bufferedReader().readText())
+            showComic(comicObject)
+
+        } catch (e: Exception) {
+            Toast.makeText(this, "No saved comic found", Toast.LENGTH_SHORT).show()
+        }
+
         showButton.setOnClickListener {
             downloadComic(numberEditText.text.toString())
         }
 
+    }
+
+    override fun onStop() {
+        super.onStop()
+        saveComic(comicJson)
     }
 
     // Fetches comic from web as JSONObject
@@ -59,6 +78,7 @@ class MainActivity : AppCompatActivity() {
 
     // Display a comic for a given comic JSON object
     private fun showComic (comicObject: JSONObject) {
+        comicJson = comicObject
         titleTextView.text = comicObject.getString("title")
         descriptionTextView.text = comicObject.getString("alt")
         Picasso.get().load(comicObject.getString("img")).into(comicImageView)
@@ -66,7 +86,9 @@ class MainActivity : AppCompatActivity() {
 
     // Implement this function
     private fun saveComic(comicObject: JSONObject) {
-
+        val file = openFileOutput("comic.json", MODE_PRIVATE)
+        file.write(comicObject.toString().toByteArray())
+        file.close()
     }
 
 
